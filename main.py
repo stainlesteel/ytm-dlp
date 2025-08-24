da6e18aa112b5881cf9b4b90a1920f2f002cdf04
puneetd <pd4500@protonmail.com>
2025-08-24 15:56:19 -0600

    better

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ import yt_dlp
 from ytmusicapi import YTMusic
 import os
 
-if not os.path.exists('music'):
-    os.mkdir('music', 0o755)
 
 ytdls = {
     'format': 'bestaudio/best',
@@ -15,6 +13,7 @@ ytdls = {
     }],
     'outtmpl': os.path.join('music', '%(title)s.%(ext)s'),
     'keepvideo': False,
+    'quiet': True,
 }
 
 burl = 'https://music.youtube.com/watch?v='
@@ -22,9 +21,24 @@ burl = 'https://music.youtube.com/watch?v='
 ytm = YTMusic()
 
 def pls(pl):
+    answer = str(input("""A new 'music' folder will be created and all\navailable music will be downloaded there.\nContinue? [Y/n]"""))
+                 
+    if answer.lower() == 'n':
+        raise SystemExit()
+
+    if not os.path.exists('music'):
+        os.mkdir('music', 0o755)
+
     plis = ytm.get_playlist(pl)
     tracks = plis['tracks']
+
+    lens = len(tracks)
+
+    print(f'{lens} songs found.')
+    num = 0
     for track in tracks:
+        num += 1
+
         title = track['title']
         id = track['videoId']
         url = f'{burl}{id}'
@@ -33,12 +47,14 @@ def pls(pl):
             print(f"{title}.mp3 already exists.")
         else:
             with yt_dlp.YoutubeDL(ytdls) as ydl:
-                print(f'Downloading {title}')
+                print(f'[{num}/{lens}] Downloading {title}')
                 ydl.download([url])
-                print(f'Downloaded {title}')
+                print(f'[{num}/{lens}] Downloaded {title}')
 
             if os.path.exists(f'music/{title}.mp4'):
                 os.remove(f'music/{title}.mp4')
 
-
-pls('PLoIoZsCilFJINI0N41NS5xnUAU1BVdfw6')
+try:
+   pls('PLoIoZsCilFJINI0N41NS5xnUAU1BVdfw6')
+except KeyboardInterrupt:
+   raise SystemExit()
